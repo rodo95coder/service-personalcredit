df62c410d35440afb198fcc5ab8836de159d5c69
ant-lazo <ant.14sag@gmail.com>
2022-05-18 00:52:16 -0500

    service-personalcredit

---
+++ src/main/java/com/nttdata/bootcamp/BootcampServiceProductPersonalcreditApplication.java
@@ -0,0 +1,13 @@
+package com.nttdata.bootcamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootcampServiceProductPersonalcreditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootcampServiceProductPersonalcreditApplication.class, args);
+	}
+
+}
